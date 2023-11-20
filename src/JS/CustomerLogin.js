117fe23b3d9f5ae06043414ebb3443753a5848a1
Jonathan Kopuri <kjernest01@gmail.com>
2023-11-20 15:31:00 -0600

    added Customer Login

--- src/JS/CustomerLogin.js
+++ src/JS/CustomerLogin.js
@@ -1,23 +1,3 @@
-// App Logic for the Customer Account creation page
-// Contains Customer class
-
-/*
-Customer Account Creation function
-gets info from Customer account creation form to create new Customer
-
-To Pass data to customer homepage the new Customer object is added to the DB and then the customerID is passed as a URL Param 
-the Customer honmepage then queries the customer info to set the active customer 
-*/
-function createAccount() {
-  let customerName = document.getElementsByName("Name");
-  let PhoneNumber = document.getElementsByName("PhoneNUmber");
-
-  const newCustomer = new Customer(customerName, PhoneNumber);
-  //add customer to database here
-
-  //pass data to customer facing side here
-}
-
 /**
  *
  * @param {*} PhoneNumber customer phone number
@@ -32,7 +12,7 @@ function queryCustomerFromLogIn(PhoneNumber, Password) {
     if (err) throw err;
     //write query here
     con.query(
-      //QUERY - NOT FINISHED
+      //TODO QUERY - NOT FINISHED
       /**
        * Need to Join CUSTOMER_INFORMATION, CUSTOMER_TRANSACTIONS, and CUSTOMER_ORDERED_ITEMS
        * From CUSTOMER_INFORMATION we get:
@@ -60,3 +40,4 @@ function queryCustomerFromLogIn(PhoneNumber, Password) {
   });
   return customer;
 }
+export { queryCustomerFromLogIn };
