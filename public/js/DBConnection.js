ae76a66a19b145eb9e44189d3f45377bfa4373cb
Jonathan Kopuri <kjernest01@gmail.com>
2023-11-21 14:34:44 -0600

    updated package.json

--- public/js/DBConnection.js
+++ public/js/DBConnection.js
@@ -1,17 +1,17 @@
 //run this in terminal using "node DBConnection.js" to make sure you have connected to your mySQL database
 //if it throws errors look up the error to see how to properly flush priveleges
 
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 //fill in these values with your own info
 var con = mysql.createConnection({
   host: "localhost",
   user: "yourusername",
   password: "yourpassword",
-  database: "databasename"
+  database: "databasename",
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
